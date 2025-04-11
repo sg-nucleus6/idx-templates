1a1fd2a0453c2ea6d4bcabc04bb247c08b7d8212
Stephano Gabriel <sg@nucleus6.com>
2025-04-11 13:12:34 -0300

    Updating idx-template.nix

---
+++ python/idx-template.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  # Shell script that produces the final environment
+  bootstrap = ''
+    cp -rf ${./.}/files "$WS_NAME"
+    chmod -R +w "$WS_NAME"
+    mv "$WS_NAME" "$out"
+
+    # Remove the template files themselves and any connection to the template's
+    # Git repository
+    rm -rf "$out/.git" "$out/idx-template".{nix,json}
+  '';
+}
