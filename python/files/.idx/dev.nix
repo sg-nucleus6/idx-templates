35f1bf7cf333a8adaaa197cc69281b4ea225424e
sg-nucleus6 <sg@nucleus6.com>
2025-04-11 13:49:55 -0300

    Update dev.nix

--- python/files/.idx/dev.nix
+++ python/files/.idx/dev.nix
@@ -46,7 +46,7 @@
         # Example: install JS dependencies from NPM
         # npm-install = "npm install";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
+        default.openFiles = [ "README.md" ];
       };
       # Runs when the workspace is (re)started
       onStart = {
