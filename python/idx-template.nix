8a0cc30358af0d023c62e9e99e8d40d0f1408495
sg <sg@nucleus6.com>
2025-04-13 16:52:36 +0000

    Minor changes to idx-template.nix

--- python/idx-template.nix
+++ python/idx-template.nix
@@ -7,11 +7,13 @@
   bootstrap = ''
     cp -rf ${./.}/files "$WS_NAME"
     chmod -R +w "$WS_NAME"
-    mv "$WS_NAME" "$out"
+    
 
     repoURL=${repoURL} j2 ${./devNix.j2}  -o "$WS_NAME"/.idx/dev.nix
     nixfmt "$WS_NAME"/.idx/dev.nix
 
+    mv "$WS_NAME" "$out"
+    
     # Remove the template files themselves and any connection to the template's
     # Git repository
     rm -rf "$out/.git" "$out/idx-template".{nix,json}
