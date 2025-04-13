2e7dde3c03afb69bc6bfe4003628b1049f83967c
sg <sg@nucleus6.com>
2025-04-13 16:26:27 +0000

    Trying Jinja2

--- python/idx-template.nix
+++ python/idx-template.nix
@@ -1,9 +1,16 @@
-{ pkgs, git ? "", ... }: {
+{ pkgs, repoURL ? "", ... }: {
   # Shell script that produces the final environment
+  packages = [
+    pkgs.j2cli
+    pkgs.nixfmt
+  ];
   bootstrap = ''
     cp -rf ${./.}/files "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
+
+    reporURL=${repoURL} j2 ${./devNix.js}  -o "$WS_NAME"/.idx/dev.nix
+    nixfmt "$WS_NAME"/.idx/dev.nix
 
     # Remove the template files themselves and any connection to the template's
     # Git repository
