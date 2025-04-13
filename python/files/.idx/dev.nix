076bacd3e5ef450a5227e1b10ca112a95803b614
sg-nucleus6 <sg@nucleus6.com>
2025-04-13 12:42:15 -0300

    Update dev.nix

--- python/files/.idx/dev.nix
+++ python/files/.idx/dev.nix
@@ -1,6 +1,6 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, repoURL ? "https://github.com/Kinto/kinto", ... }: {
+{ pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
